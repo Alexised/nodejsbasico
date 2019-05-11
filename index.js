df7398eaf410ccda84cdc6696bf4a065960cc0d5
Alexised <edwinalexis1996@hotmail.com>
2019-05-11 15:41:20 -0500

    asycn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+//call.greet("alexis")
+
+async function callWithPromise(){
+    const fullName = await call.withPromise("alexis","rodriguez")
+    console.log(fullName)
+}                           
+
+callWithPromise()
