795c7808ca0fe236041ff25ccfc722ac14622f93
Alexised <edwinalexis1996@hotmail.com>
2019-05-11 17:36:41 -0500

    fs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require("fs");
+const ops = require("./src/fileops");
+
+let incrementValue;
+fs.readFile("./resources/number.txt","utf8",(err,text)=>{
+    if(err) throw err;
+    const numbers  =text.split("\r\n").map(n=>Number(n))
+    incrementValue =ops.incrementValues(numbers)
+        fs.writeFile("./resources/numbernew.txt",incrementValue.join("\r\n"),(err,result)=>{
+            if(err) throw err;
+            
+        
+        })
+});
+
