bfbb15971241910df38f38b01ebecbedcdc79b90
Alexised <edwinalexis1996@hotmail.com>
2019-05-16 20:16:47 -0500

    express

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express")
+const app = express();
+
+app.get("/app",(req,res)=>{
+    res.send("estoy en la ruta app")
+})
+
+app.get("/",(req,res)=>{
+    res.send("estoy en la ruta home")
+})
+app.get("*",(req,res)=>{
+    res.send("estoy en la ruta null")
+})
+app.listen(3000,()=>{
+    console.log("el servidor esta iniciado port 3000")
+})
